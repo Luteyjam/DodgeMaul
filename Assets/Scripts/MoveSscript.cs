984991a5c2bbee0df818fd1e4ad0c95d70569439
James Lutey <jace.lutey@gmail.com>
2016-09-17 13:04:23 -0400

    Added a script for movement
    (It doesn't work yet)

--- Assets/Scripts/MoveSscript.cs
+++ Assets/Scripts/MoveSscript.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class MoveSscript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    protected new Rigidbody2D rigidbody;
+    private float speed = 25;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponentInParent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        //Movement
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(new Vector2(-speed, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(new Vector2(speed, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(new Vector2(0,speed));
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(new Vector2(0, -speed));
+        }
+    }
+    
 }
