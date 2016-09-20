0a6c082958664a2c00135c8111d0ba2219f3c7eb
James Lutey <jace.lutey@gmail.com>
2016-09-19 20:49:05 -0400

    Fixed projectiles
    Projectiles will now self-destruct upon bouncing 3 times.

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour {
+
+    public new Rigidbody2D rigidbody;
+    public new GameObject o;
+    public float count;
+    bool isColliding;
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+        o = this.gameObject;
+        count = 0;
+        Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(), GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>(), true);
+        isColliding = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        isColliding = false;
+	if(count >= 3)
+        {
+            Destroy(o);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (isColliding)
+        {
+            return;
+        }
+        isColliding = true;
+        if(coll.gameObject.tag == "Barrier")
+        {
+            count += 1;
+        }
+        if (coll.gameObject.tag == "player")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
